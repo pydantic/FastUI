792a7c4761dd78df829accc9353a78d67091cae7
Samuel Colvin <s@muelcolvin.com>
2023-11-24 10:26:35 +0000

    move scss

--- packages/vanilla/src/main.tsx
+++ packages/vanilla/src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import App from './App'
-import 'fastui-bootstrap/main.scss'
+import './main.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
