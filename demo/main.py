d1465721357442bfea184e270bbf9a289410352a
Samuel Colvin <s@muelcolvin.com>
2023-12-07 12:07:56 +0300

    moving from samuelcolvin -> pydantic org

--- demo/main.py
+++ demo/main.py
@@ -13,8 +13,8 @@ router = APIRouter()
 def api_index() -> list[AnyComponent]:
     # language=markdown
     markdown = """\
-This site providers a demo of [FastUI](https://github.com/samuelcolvin/FastUI), the code for the demo
-is [here](https://github.com/samuelcolvin/FastUI/tree/main/demo).
+This site providers a demo of [FastUI](https://github.com/pydantic/FastUI), the code for the demo
+is [here](https://github.com/pydantic/FastUI/tree/main/demo).
 
 The following components are demonstrated:
 
