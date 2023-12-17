5d790e9389a3fd3222f8305dc89ba12301ba34cc
Samuel Colvin <s@muelcolvin.com>
2023-12-17 18:03:20 +0000

    add video to demo index

--- demo/main.py
+++ demo/main.py
@@ -32,6 +32,7 @@ The following components are demonstrated:
 * `ServerLoad` — see [dynamic modal example](/components#dynamic-modal) and [SSE example](/components#server-load-sse)
 * `Image` - example [here](/components#image)
 * `Iframe` - example [here](/components#iframe)
+* `Video` - example [here](/components#video)
 * `Table` — See [cities table](/table/cities) and [users table](/table/users)
 * `Pagination` — See the bottom of the [cities table](/table/cities)
 * `ModelForm` — See [forms](/forms/login)
