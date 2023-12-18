1303964b499f168627b3fa000745a0d12c3583d3
Samuel Colvin <s@muelcolvin.com>
2023-12-18 18:10:51 +0000

    better schema generation

--- gen_schema.py
+++ gen_schema.py
@@ -1,19 +1,14 @@
 from pathlib import Path
-from typing import Literal
-import typing_extensions
-
-from pydantic import BaseModel
+from typing import Any
 
 from fastui import FastUI
-from fastui import class_name as _class_name
-from fastui import events
 from pydantic.json_schema import GenerateJsonSchema, JsonSchemaValue
 from pydantic_core import to_json, core_schema
 
 
 def is_type_schema(schema: core_schema.WithDefaultSchema) -> bool:
     inner = schema['schema']
-    if inner.get('type') == 'literal':
+    if inner['type'] == 'literal':
         expected = inner['expected']
         if len(expected) == 1 and expected[0] == schema['default']:
             return True
@@ -21,27 +16,16 @@ def is_type_schema(schema: core_schema.WithDefaultSchema) -> bool:
 
 
 class CustomGenerateJsonSchema(GenerateJsonSchema):
-    # def generate_inner(self, schema) -> JsonSchemaValue:
-    #     # debug(schema['type'], schema.keys())
-    #     if schema.get('type') == 'tagged-union':
-    #         if 'Div' in schema['choices'] and 'Text' in schema['choices']:
-    #             schema['ref'] = 'fastui.components.AnyComponent'
-    #     # debug(schema)
-    #     return super().generate_inner(schema)
-
     def field_title_should_be_set(self, schema) -> bool:
         return False
-        # if schema.get('type') == 'literal' and schema.get('expected') == [IsStr(regex='[A-Z].+')]:
-        #     return False
-        # return super().field_title_should_be_set(schema)
 
     def default_schema(self, schema: core_schema.WithDefaultSchema) -> JsonSchemaValue:
         inner = schema['schema']
         if is_type_schema(schema):
             return self.generate_inner(inner)
 
-        if inner.get('type') == 'nullable' and inner.get('ref', '').startswith('fastui.class_name.ClassName:'):
-            return self.generate_inner(inner)
+        # if inner.get('type') == 'nullable' and inner.get('ref', '').startswith('fastui.class_name.ClassName:'):
+        #     return self.generate_inner(inner)
 
         return super().default_schema(schema)
 
@@ -69,29 +53,29 @@ class CustomGenerateJsonSchema(GenerateJsonSchema):
         if schema['discriminator'] == 'type':
             if 'go-to' in schema['choices']:
                 schema['ref'] = 'fastui.events.AnyEvent'
-            # elif 'Div' in choice_keys and 'Text' in choice_keys:
-            #     schema['ref'] = 'fastui.components.AnyComponent'
-
         return super().tagged_union_schema(schema)
 
 
-# Thing = typing_extensions.TypeAliasType('Thing', Literal['Div', 'Text'])
-Thing = Literal['Div', 'Text']
-
-
-class MyModel(BaseModel):
-    x: int
-    # type: Literal['MyModel'] = 'MyModel'
-    t: Thing
-    # class_name: _class_name.ClassNameField = None
-    # trigger: events.AnyEvent = None
-
-
-# test_schema = MyModel.model_json_schema(by_alias=True, mode='serialization', schema_generator=CustomGenerateJsonSchema)
-# debug(test_schema)
-
-
 fastui_schema = FastUI.model_json_schema(by_alias=True, mode='serialization', schema_generator=CustomGenerateJsonSchema)
+any_comp_def = fastui_schema['$defs']['Div']['properties']['components']['items'].copy()
+any_comp_ref = {'$ref': '#/$defs/AnyComponent'}
+
+
+def replace_any_comp(s: Any):
+    if isinstance(s, dict):
+        if s == any_comp_def:
+            return any_comp_ref
+        else:
+            return {k: replace_any_comp(v) for k, v in s.items()}
+    elif isinstance(s, list):
+        return [replace_any_comp(v) for v in s]
+    else:
+        return s
+
+
+fastui_schema['items'] = any_comp_ref
+fastui_schema = replace_any_comp(fastui_schema)
+fastui_schema['$defs']['AnyComponent'] = any_comp_def
 Path('schema.json').write_bytes(to_json(fastui_schema, indent=2))
 
 """
