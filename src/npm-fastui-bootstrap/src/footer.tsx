4b336587954c5bb86108dae227ee8e6f3e13f79f
Dominik Vogt <dominik.vogt@gmail.com>
2023-12-19 20:52:29 +0100

    Removed horizontal line in footer

--- src/npm-fastui-bootstrap/src/footer.tsx
+++ src/npm-fastui-bootstrap/src/footer.tsx
@@ -6,21 +6,17 @@ export const Footer: FC<components.FooterProps> = (props) => {
     link.mode = link.mode || 'footer'
     return link
   })
-  const extraSeparatorProp = useClassName(props, { el: 'extra-separator' })
   const extraProp = useClassName(props, { el: 'extra' })
   return (
-    <div className={useClassName(props, { el: 'contents' })}>
-      <footer className={useClassName(props)}>
-        <ul className={useClassName(props, { el: 'link-list' })}>
-          {links.map((link, i) => (
-            <li className="nav-item">
-              <components.LinkComp key={i} {...link} />
-            </li>
-          ))}
-        </ul>
-        {props.extraText && props.links.length > 0 && <div className={extraSeparatorProp} />}
-        {props.extraText && <div className={extraProp}>{props.extraText}</div>}
-      </footer>
-    </div>
+    <footer className={useClassName(props, { el: 'contents' })}>
+      <ul className={useClassName(props, { el: 'link-list' })}>
+        {links.map((link, i) => (
+          <li className="nav-item">
+            <components.LinkComp key={i} {...link} />
+          </li>
+        ))}
+      </ul>
+      {props.extraText && <div className={extraProp}>{props.extraText}</div>}
+    </footer>
   )
 }
