3c586588d4cf03104cabe971ee2c8d43dfed5b1b
sergue1 <serguei.strigo@gmail.com>
2024-02-15 15:37:38 +0100

    naming

--- src/python-fastui/fastui/json_schema.py
+++ src/python-fastui/fastui/json_schema.py
@@ -312,8 +312,7 @@ def deference_json_schema(
         if def_schema is None:
             raise ValueError(f'Invalid $ref "{ref}", not found in {defs}')
         else:
-            # clone dict to avoid attribute leakage
-            return def_schema.copy(), required
+            return def_schema.copy(), required  # clone dict to avoid attribute leakage via shared schema
     elif any_of := schema.get('anyOf'):
         if len(any_of) == 2 and sum(s.get('type') == 'null' for s in any_of) == 1:
             # If anyOf is a single type and null, then it is optional
