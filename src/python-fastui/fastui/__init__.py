3f2b9c14a19ea456fc819ef94c87aca0d00dea6a
Victorien <65306057+Viicos@users.noreply.github.com>
2025-09-03 10:41:15 +0200

    Prepare release v0.8.0 (#380)

--- src/python-fastui/fastui/__init__.py
+++ src/python-fastui/fastui/__init__.py
@@ -4,7 +4,7 @@ import pydantic
 
 from .components import AnyComponent
 
-__version__ = '0.7.0'
+__version__ = '0.8.0'
 __all__ = 'AnyComponent', 'FastUI', 'prebuilt_html'
 
 
@@ -23,7 +23,7 @@ class FastUI(pydantic.RootModel):
             return [v]
 
 
-_PREBUILT_VERSION = '0.0.24'
+_PREBUILT_VERSION = '0.0.25'
 _PREBUILT_CDN_URL = f'https://cdn.jsdelivr.net/npm/@pydantic/fastui-prebuilt@{_PREBUILT_VERSION}/dist/assets'
 
 
